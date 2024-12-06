d4339cc8703d998a9239afee54d3649d83a6bcf1
huyn3195 <huyn3195@stthomas.edu>
2024-12-06 12:14:23 -0600

    Fixing dependency

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   const [background, setBackground] = useState(
